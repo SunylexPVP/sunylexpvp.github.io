c2b874c3da3a372f472d975e46059da8fe696ea7
SunylexPVP <112217847+SunylexPVP@users.noreply.github.com>
2022-09-14 18:32:27 -0400

    Delete options.js

--- options.js
+++
@@ -1,7 +0,0 @@
-title = "Sunylex";
-favicon = "./file/logo.jpg";
-maxPageWidth = "2000px";
-pageFont = "arial,sans-serif";
-pageBackgroundColor = "#d3d399";
-serverIp = "Sunylex.minehut.gg";
-serverPort = "25565";
